72ebac78c94dbc6d96cd79cc2a1a591ce4704124
venkatch51 <venkatch8051@gmail.com>
2024-09-07 21:10:42 +0530

    text-align-update

--- src/Main/HomeSection.js
+++ src/Main/HomeSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HomeSection.css";
-
+import '../images/hero-img.jpg'
 const HomeSection = () => {
   return (
     <div className="bg-color">
@@ -8,8 +8,8 @@ const HomeSection = () => {
         <div className="row align-items-center">
           <div className="col-md-6">  
             <h6 className="fade-up-name text-light">Jithendra Chitneedi</h6>
-            <h3 className="text-light">HAY IAM JANNA! WEB DEVELOPER</h3>
-            <p className="text-light">
+            <h3 className="text-light text-start">HAY IAM JANNA! WEB DEVELOPER</h3>
+            <p className="text-light text-start">
               Yet bed any for travelling assistance indulgence unpleasing. Not
               thoughts all exercise blessing. Indulgence way everything joy
               alteration boisterous the attachment.
@@ -22,7 +22,7 @@ const HomeSection = () => {
             </div>
           </div>
           <div className="col-md-6">
-            <img className="pic" src="https://janna-react.vercel.app/images/home-banner.png" />
+            <img className="pic" src='../images/hero-img.jpg' alt="banner-image"/>
           </div>
         </div>
       </div>
