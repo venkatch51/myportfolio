f868fb62dd657dfaa4cfabd0dabf78837d0a115a
Jitendrp <jithendravenkat365@gmail.com>
2025-03-26 10:36:01 +0530

    updated the contact content

--- src/Main/ContactSection.js
+++ src/Main/ContactSection.js
@@ -62,7 +62,7 @@ const ContactSection = () => {
               Always Avaialable for freelancing if the right project come in
               Feel Free to Contact me
             </p>
-            <h1 className="text-light">Contact Form coming soon!</h1>
+            <h1 className="text-light">Please Contact to Know More</h1>
             <form class="row g-3 contact-form">
               <div class="col-md-6">
                 <label for="inputEmail4" class="form-label">
